45d568e09f61023a7e123f84194d6e0d17fe11f9
Roxirore <roxana.rod.rev@gmail.com>
2023-01-11 22:24:43 -0500

    partes unidas

--- img/main.js
+++
@@ -1,6 +0,0 @@
-const toggleMenuElement = document.querySelector('#toggle-menu');
-const mainMenuElement = document.querySelector('#main-menu');
-
-toggleMenuElement.addEventListener('click', () => {
-    mainMenuElement.classList.toggle('main-menu--show');
-});
